a1043c719dd8865b766d1ff26bd86f8cbbefd25b
chengyuanlong <chengyuanlong@rsung.com>
2018-12-07 15:47:40 +0800

    添加 phpint

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.44 on 2018-12-07 06:55:27.
+ * Generated for Laravel 5.5.44 on 2018-12-07 07:45:39.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
