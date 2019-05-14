0ec8b7d62760fec460178bdb3d6b0d33a0402e69
chengyuanlong <377304925@qq.com>
2019-05-14 18:30:08 +0800

    加入bootstrap缓存

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,48 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'overtrue/laravel-wechat' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Overtrue\\LaravelWeChat\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'EasyWeChat' => 'Overtrue\\LaravelWeChat\\Facade',
+    ),
+  ),
+  'rapide/laravel-queue-kafka' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Rapide\\LaravelQueueKafka\\LaravelQueueKafkaServiceProvider',
+    ),
+  ),
+  'sjje/swaggervel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Sjje\\Swaggervel\\SwaggervelServiceProvider',
+    ),
+  ),
+);
